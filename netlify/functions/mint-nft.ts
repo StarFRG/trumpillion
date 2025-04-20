7aa3d5ac4d619a34602c06460493bb55a0c7b5bf
Murat Güngör <muratgungor@murats-imac.home>
2025-04-20 19:26:15 +0200

    Fix: Netlify mint-nft setup und createUmi via umi-bundle-defaults

--- netlify/functions/mint-nft.ts
+++ netlify/functions/mint-nft.ts
@@ -1,7 +1,7 @@
 import { Handler } from '@netlify/functions';
 import { createUmi } from '@metaplex-foundation/umi-bundle-defaults';
 import { createSignerFromKeypair, generateSigner } from '@metaplex-foundation/umi';
-import { createKeypairFromSecretKey } from '@metaplex-foundation/umi-bundle-defaults';
+import { createKeypairFromSecretKey } from '@metaplex-foundation/umi';
 import { signerIdentity } from '@metaplex-foundation/umi';
 import { irysUploader } from '@metaplex-foundation/umi-uploader-irys';
 import { TokenStandard, createV1, mplTokenMetadata } from '@metaplex-foundation/mpl-token-metadata';
