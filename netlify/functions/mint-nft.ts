035c7ee6d455cc23443069eb95da35ae70fd4a12
Murat Güngör <muratgungor@murats-imac.home>
2025-04-21 08:07:50 +0200

    Fix: Entferne web3JsRpc und registriere mplTokenMetadata korrekt

--- netlify/functions/mint-nft.ts
+++ netlify/functions/mint-nft.ts
@@ -5,7 +5,6 @@ import {
   createKeypairFromSecretKey,
   signerIdentity
 } from '@metaplex-foundation/umi';
-import { web3JsRpc } from '@metaplex-foundation/umi-rpc-web3js';
 import { mplTokenMetadata } from '@metaplex-foundation/mpl-token-metadata';
 import { irysUploader } from '@metaplex-foundation/umi-uploader-irys';
 import { TokenStandard, createV1 } from '@metaplex-foundation/mpl-token-metadata';
@@ -19,7 +18,6 @@ if (!process.env.SOLANA_RPC_URL?.startsWith('http')) {
 
 // Initialize Umi with RPC
 const umi = createUmi(process.env.SOLANA_RPC_URL)
-  .use(web3JsRpc({ rpcEndpoint: process.env.SOLANA_RPC_URL }))
   .use(mplTokenMetadata())
   .use(irysUploader());
 
