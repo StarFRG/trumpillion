8dd1914bd6c0449df3c904aa6f08488efcbdd3c9
Murat Güngör <muratgungor@murats-imac.home>
2025-04-27 18:04:36 +0200

    Fix: Trezor entfernt und Build gefixt

--- vite.config.ts
+++ vite.config.ts
@@ -70,6 +70,7 @@ export default defineConfig({
     'process.env': process.env
   },
   optimizeDeps: {
+    exclude: ['@trezor/connect-web', '@trezor/connect-common'],
     esbuildOptions: {
       define: {
         global: 'globalThis'
@@ -86,11 +87,10 @@ export default defineConfig({
   server: {
     host: true,
     port: 5173,
-    strictPort: true,
+    strictPort: false,
     hmr: {
       timeout: 60000,
       overlay: true,
-      clientPort: 5173,
       protocol: 'ws'
     },
     watch: {
@@ -101,15 +101,17 @@ export default defineConfig({
   preview: {
     host: true,
     port: 5173,
-    strictPort: true
+    strictPort: false
   },
   build: {
     target: 'esnext',
     sourcemap: true,
     commonjsOptions: {
-      transformMixedEsModules: true
+      transformMixedEsModules: true,
+      exclude: ['@trezor/connect-web/**', '@trezor/connect-common/**']
     },
     rollupOptions: {
+      external: ['@trezor/connect-web', '@trezor/connect-common'],
       output: {
         manualChunks: {
           vendor: ['react', 'react-dom'],
